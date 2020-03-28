3a12dd1d3dea5bec8838adfa796ccf42e4ba445b
Jack Antcio <anticoj@bc.edu>
2020-03-28 15:42:44 -0400

    Updates

---
+++ precache-manifest.f6ae2301d0d133f11ede8d02207d8c71.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a09828b6e877a5244d162c007a4ff3e",
+    "url": "/theproductmangementintern.github.io/index.html"
+  },
+  {
+    "revision": "37d21833ac7ac52eaceb",
+    "url": "/theproductmangementintern.github.io/static/css/main.76c930e1.chunk.css"
+  },
+  {
+    "revision": "de8412a7dd52013c6ee0",
+    "url": "/theproductmangementintern.github.io/static/js/2.1c9a4f90.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/theproductmangementintern.github.io/static/js/2.1c9a4f90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37d21833ac7ac52eaceb",
+    "url": "/theproductmangementintern.github.io/static/js/main.56a78458.chunk.js"
+  },
+  {
+    "revision": "c5bdedeb76965ae9b3cf",
+    "url": "/theproductmangementintern.github.io/static/js/runtime-main.831c391f.js"
+  }
+]);
