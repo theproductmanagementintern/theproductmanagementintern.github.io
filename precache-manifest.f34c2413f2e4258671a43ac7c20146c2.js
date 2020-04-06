ec4da774ec6495fcead248821ab9de1492f8bc87
Jack Antcio <anticoj@bc.edu>
2020-04-06 11:25:57 -0400

    Updates

---
+++ precache-manifest.f34c2413f2e4258671a43ac7c20146c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6206dbfae3461da093921bfa8271e0ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9866e9a889cc0f8d6793",
+    "url": "/static/css/main.3a403be2.chunk.css"
+  },
+  {
+    "revision": "de8412a7dd52013c6ee0",
+    "url": "/static/js/2.1c9a4f90.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.1c9a4f90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9866e9a889cc0f8d6793",
+    "url": "/static/js/main.f0e15186.chunk.js"
+  },
+  {
+    "revision": "a04e81571ba0c10558e3",
+    "url": "/static/js/runtime-main.1b082286.js"
+  }
+]);
