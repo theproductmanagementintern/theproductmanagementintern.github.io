952a17be9278c1967442f0da8c37edf38e8ee844
Jack Antcio <anticoj@bc.edu>
2020-04-09 11:37:46 -0400

    Added firebase to site

--- src/index.js
+++ src/index.js
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import * as firebase from 'firebase';
+
+var firebaseConfig = {
+    apiKey: "AIzaSyAXBrh23nRfrx8e9x1quhaA4bGnmYFCQYo",
+    authDomain: "the-product-management-intern.firebaseapp.com",
+    databaseURL: "https://the-product-management-intern.firebaseio.com",
+    projectId: "the-product-management-intern",
+    storageBucket: "the-product-management-intern.appspot.com",
+    messagingSenderId: "744758020904",
+    appId: "1:744758020904:web:002db8166945992dcc1e1d",
+    measurementId: "G-L1ZK853X0P"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
